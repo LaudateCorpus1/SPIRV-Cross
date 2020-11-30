7b3f06f6a22f658ca3c01bbf5e9640033ff99454
Lukas Hermanns <lukas.hermanns@epicgames.com>
2020-11-30 13:59:51 -0500

    MSL: Reset state whether base vertex/instance is needed.

--- spirv_msl.cpp
+++ spirv_msl.cpp
@@ -1433,6 +1433,11 @@ string CompilerMSL::compile()
 		for (auto &id : next_metal_resource_ids)
 			id = 0;
 
+		// UE Change Begin: Reset state whether base vertex/instance is needed
+		needs_base_vertex_arg = TriState::Neutral;
+		needs_base_instance_arg = TriState::Neutral;
+		// UE Change End: Reset state whether base vertex/instance is needed
+
 		// Move constructor for this type is broken on GCC 4.9 ...
 		buffer.reset();
 
@@ -14021,10 +14026,8 @@ string CompilerMSL::builtin_to_glsl(BuiltIn builtin, StorageClass storage)
 		{
 			if (builtin_declaration)
 			{
-				// UE Change Begin: Only change tri-states once to not break condition on recompilation.
-				if (needs_base_vertex_arg == TriState::Neutral)
+				if (needs_base_vertex_arg != TriState::No)
 					needs_base_vertex_arg = TriState::Yes;
-				// UE Change End: Only change tri-states once to not break condition on recompilation.
 				return "gl_VertexID";
 			}
 			else
@@ -14044,10 +14047,8 @@ string CompilerMSL::builtin_to_glsl(BuiltIn builtin, StorageClass storage)
 		{
 			if (builtin_declaration)
 			{
-				// UE Change Begin: Only change tri-states once to not break condition on recompilation.
-				if (needs_base_instance_arg == TriState::Neutral)
+				if (needs_base_instance_arg != TriState::No)
 					needs_base_instance_arg = TriState::Yes;
-				// UE Change End: Only change tri-states once to not break condition on recompilation.
 				return "gl_InstanceID";
 			}
 			else
@@ -14067,10 +14068,8 @@ string CompilerMSL::builtin_to_glsl(BuiltIn builtin, StorageClass storage)
 		{
 			if (builtin_declaration)
 			{
-				// UE Change Begin: Only change tri-states once to not break condition on recompilation.
-				if (needs_base_vertex_arg == TriState::Neutral)
+				if (needs_base_vertex_arg != TriState::No)
 					needs_base_vertex_arg = TriState::Yes;
-				// UE Change End: Only change tri-states once to not break condition on recompilation.
 				return "gl_VertexIndex";
 			}
 			else
@@ -14090,10 +14089,8 @@ string CompilerMSL::builtin_to_glsl(BuiltIn builtin, StorageClass storage)
 		{
 			if (builtin_declaration)
 			{
-				// UE Change Begin: Only change tri-states once to not break condition on recompilation.
-				if (needs_base_instance_arg == TriState::Neutral)
+				if (needs_base_instance_arg != TriState::No)
 					needs_base_instance_arg = TriState::Yes;
-				// UE Change End: Only change tri-states once to not break condition on recompilation.
 				return "gl_InstanceIndex";
 			}
 			else
@@ -14110,10 +14107,7 @@ string CompilerMSL::builtin_to_glsl(BuiltIn builtin, StorageClass storage)
 		if (msl_options.supports_msl_version(1, 1) &&
 		    (msl_options.ios_support_base_vertex_instance || msl_options.is_macos()))
 		{
-			// UE Change Begin: Only change tri-states once to not break condition on recompilation.
-			if (needs_base_vertex_arg == TriState::Neutral)
-				needs_base_vertex_arg = TriState::No;
-			// UE Change End: Only change tri-states once to not break condition on recompilation.
+			needs_base_vertex_arg = TriState::No;
 			return "gl_BaseVertex";
 		}
 		else
@@ -14124,10 +14118,7 @@ string CompilerMSL::builtin_to_glsl(BuiltIn builtin, StorageClass storage)
 		if (msl_options.supports_msl_version(1, 1) &&
 		    (msl_options.ios_support_base_vertex_instance || msl_options.is_macos()))
 		{
-			// UE Change Begin: Only change tri-states once to not break condition on recompilation.
-			if (needs_base_instance_arg == TriState::Neutral)
-				needs_base_instance_arg = TriState::No;
-			// UE Change End: Only change tri-states once to not break condition on recompilation.
+			needs_base_instance_arg = TriState::No;
 			return "gl_BaseInstance";
 		}
 		else
